a277ce71f056ee081d17d2f2ee2e168dd79afe3b
성유진 <syj010820@naver.com>
2024-11-04 22:16:18 +0900

    fix: shell entity nullable 처리

--- be/src/shell/shell.entity.ts
+++ be/src/shell/shell.entity.ts
@@ -8,24 +8,25 @@ export class Shell {
   @Column({ type: 'bigint' })
   shellId: number;
 
-  @Column()
+  // 임시로 nullable 처리
+  @Column({ nullable: true })
   sessionId: string;
 
-  @Column()
+  @Column({ nullable: true })
   runTime: string;
 
-  @Column()
+  @Column({ nullable: true })
   queryType: string;
 
-  @Column()
+  @Column({ nullable: true })
   failMessage: string;
 
-  @Column()
+  @Column({ nullable: true })
   affectedRows: number;
 
-  @Column({ type: 'json' })
+  @Column({ type: 'json', nullable: true })
   resultTable: object;
 
-  @Column()
+  @Column({ nullable: true })
   queryStatus: boolean;
 }
