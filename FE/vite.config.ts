b77c026141b03eb2527dcdd5272c71fd9f9cd8a7
KimDayeong <dyyukim@gmail.com>
2024-11-05 11:14:58 +0900

    chore: svgr 제거

--- FE/vite.config.ts
+++ FE/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsConfigPaths from 'vite-tsconfig-paths'
-import svgr from 'vite-plugin-svgr'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react(), tsConfigPaths(), svgr()],
+  plugins: [react(), tsConfigPaths()],
   build: {
     target: 'esnext',
   },
