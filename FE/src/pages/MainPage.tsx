e2a3f5a28a611c8cc77ab4405d6272f834dceeab
KimDayeong <dyyukim@gmail.com>
2024-11-05 13:07:53 +0900

    style: 전체 레이아웃 스타일 수정

--- FE/src/pages/MainPage.tsx
+++ FE/src/pages/MainPage.tsx
@@ -5,6 +5,7 @@ import * as React from 'react'
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 import { Badge } from '@/components/ui/badge'
 import AppSidebar from '@/components/AppSidebar'
+import Shell from '@/components/Shell'
 
 export default function Page() {
   return (
@@ -17,13 +18,17 @@ export default function Page() {
     >
       <AppSidebar />
       <SidebarInset>
-        <header className="sticky top-0 flex shrink-0 items-center gap-2 border-b bg-background p-4">
-          <h2 className="h-full text-base BoldM-none">Q-Lab</h2>
+        <header className="sticky top-0 flex shrink-0 items-center gap-2 border-b bg-background p-3.5">
+          <h2 className="h-full text-xl font-bold text-foreground">Q-Lab</h2>
         </header>
         <div className="sticky top-0 flex shrink-0 items-center gap-3 border-b bg-background p-2">
-          <Badge variant="outline">+ add shell</Badge>
+          <Badge variant="outline" className="cursor-pointer">
+            + add
+          </Badge>
         </div>
-        <div className="flex flex-1 flex-col gap-4 p-4" />
+        <div className="flex flex-1 flex-col gap-4 p-4">
+          <Shell />
+        </div>
       </SidebarInset>
     </SidebarProvider>
   )
