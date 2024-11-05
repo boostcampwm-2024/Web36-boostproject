1df7aaec566a420ef936c5bab2b0045c3aa0ca0e
KimDayeong <dyyukim@gmail.com>
2024-11-05 13:08:18 +0900

    feat: shell 컴포넌트 레이아웃  추가

--- FE/src/components/Shell.tsx
+++ FE/src/components/Shell.tsx
@@ -1,5 +1,16 @@
 import * as React from 'react'
+import PlayCircle from '@/assets/play_circle.svg'
 
 export default function Shell() {
-  return <div className="aspect-video h-12 w-full rounded-lg bg-muted/50" />
+  return (
+    <div className="flex h-12 w-full items-center overflow-hidden rounded-lg bg-secondary">
+      <button type="button" className="bg-primary p-3">
+        <img src={PlayCircle} alt="play button" />
+      </button>
+      <input
+        type="text"
+        className="ml-3 h-8 w-full border-none bg-secondary p-2 text-base font-medium text-foreground focus:outline-none"
+      />
+    </div>
+  )
 }
