03f3e5def8171647d00d304aa3628b02215f5b32
KimDayeong <dyyukim@gmail.com>
2024-11-06 11:34:51 +0900

    feat: 쉘 컴포넌트 추가

--- FE/src/components/Shell.tsx
+++ FE/src/components/Shell.tsx
@@ -1,7 +1,37 @@
 import * as React from 'react'
 import PlayCircle from '@/assets/play_circle.svg'
+import ShellType from '@/types/interfaces'
 
-export default function Shell() {
+export default function Shell({ shell }: { shell: ShellType }) {
+  const {
+    shellId,
+    queryStatus,
+    runTime,
+    query,
+    queryType,
+    failMessage,
+    affectedRows,
+    table,
+  } = shell
+
+  const successMessage = `Query OK ${affectedRows} affected (${runTime} sec)`
+
+  const plusButton = (
+    <button type="button" className="flex w-full justify-center bg-primary">
+      <div className="text-bold w-3 text-background">+</div>
+    </button>
+  )
+
+  const resultBox = (
+    <div className="flex w-full flex-col overflow-hidden rounded-sm bg-secondary">
+      <div className="m-3">
+        <p>{queryStatus ? successMessage : failMessage}</p>
+        {queryType === 'SELECT' && <p>{JSON.stringify(table)}</p>}
+      </div>
+      {queryType === 'SELECT' && plusButton}
+    </div>
+  )
+
   return (
     <>
       <div className="flex h-12 w-full items-center overflow-hidden rounded-sm bg-secondary">
@@ -10,17 +40,11 @@ export default function Shell() {
         </button>
         <input
           type="text"
+          defaultValue={query ?? ''}
           className="ml-3 h-8 w-full border-none bg-secondary p-2 text-base font-medium text-foreground focus:outline-none"
         />
       </div>
-      <div className="flex w-full flex-col overflow-hidden rounded-sm bg-secondary">
-        <div className="m-3">
-          <p>this is result</p>
-        </div>
-        <button type="button" className="flex w-full justify-center bg-primary">
-          <div className="text-bold w-3 text-background">+</div>
-        </button>
-      </div>
+      {runTime != null && resultBox}
     </>
   )
 }
