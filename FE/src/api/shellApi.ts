03b635ab976a3cc20d6afe414f3f36507ee3d4cc
KimDayeong <dyyukim@gmail.com>
2024-11-07 16:21:29 +0900

    refactor: shell api를 함수 표현식으로 변경

--- FE/src/api/shellApi.ts
+++ FE/src/api/shellApi.ts
@@ -1,28 +1,26 @@
 import axiosClient from '@/api/axiosClient'
 
-export const fetchShells = async () => {
+export async function fetchShells() {
   const response = await axiosClient.get('/shells')
   return response.data
 }
 
-export const addShell = async (shellId: number) => {
-  const response = await axiosClient.post('/shells', {
-    shellId,
-  })
+export async function addShell(shellId: number) {
+  const response = await axiosClient.post('/shells', { shellId })
   return response.data
 }
 
-export const deleteShell = async (shellId: number) => {
+export async function deleteShell(shellId: number) {
   const response = await axiosClient.delete(`/shells/${shellId}`)
   return response.data
 }
 
-export const updateShell = async (shellId: number, query: string) => {
+export async function updateShell(shellId: number, query: string) {
   const response = await axiosClient.put(`/shells/${shellId}`, { query })
   return response.data
 }
 
-export const saveShellTable = async (shellId: number, table: unknown[]) => {
+export async function saveShellTable(shellId: number, table: unknown[]) {
   const response = await axiosClient.post(`/shells/${shellId}/table`, { table })
   return response.data
 }
