f30e38741671d0be28edce442d93ec3a6c198911
성유진 <syj010820@naver.com>
2024-11-07 13:22:54 +0900

    feat: session storage redis로 변경

--- be/src/session/session.middleware.ts
+++ be/src/session/session.middleware.ts
@@ -1,26 +1,34 @@
 import session from 'express-session';
-import createMemoryStore from 'memorystore';
 import dotenv from 'dotenv';
 import { NextFunction, Request, Response } from 'express';
 import { NestMiddleware } from '@nestjs/common';
 import { v4 as uuidv4 } from 'uuid';
 import cookieParser from 'cookie-parser';
+import Redis from 'ioredis';
+import RedisStore from 'connect-redis';
 
 dotenv.config();
 
 export class SessionMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     cookieParser()(req, res, () => {
-      const store = createMemoryStore(session);
       const sessionId = req.cookies?.sid;
+
+      const redisClient = new Redis({
+        host: process.env.REDIS_HOST,
+        port: parseInt(process.env.REDIS_PORT),
+      });
 
       session({
         secret: process.env.SESSION_SECRET,
         resave: false,
         saveUninitialized: false,
-        store: new store({ checkPeriod: 1000 * 60 * 60 }), // 1시간
+        store: new RedisStore({ client: redisClient }),
         genid: () => {
           const uuid = 'db' + uuidv4().replace(/[^a-zA-Z0-9]/g, '');
+          if (!sessionId) {
+            redisClient.set(uuid, '', 'EX', 60 * 60);
+          }
           return sessionId || uuid;
         },
         cookie: {
