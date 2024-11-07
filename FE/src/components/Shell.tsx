1fb795e356e85b1288b40eea57b77017c63ed314
Znero <165549165+znero1022@users.noreply.github.com>
2024-11-07 10:19:00 +0900

    [#9] 추가버튼을 통해 쉘을 추가한다 (#56)
    * style: secondary 색상 변경
    
    * feat: 상수 파일 추가
    
    * feat: 쉘 객체 인터페이스 정의
    
    * feat: 사이드바 컴포넌트가 상수 파일 정보를 가져오게 변경
    
    * refactor: 쉘 객체 인터페이스 수정
    
    * feat: 쉘 추가 기능 구현
    
    * feat: 목업 데이터 추가
    
    * feat: 쉘 데이터 타입 인터페이스 수정
    
    * feat: 쉘 리스트 컴포넌트 추가
    
    * feat: 메인페이지에서 쉘 리스트 컴포넌트 분리
    
    * feat: 쉘 컴포넌트 추가
    
    * chore: vite 환경설정파일 수정

--- FE/src/components/Shell.tsx
+++ FE/src/components/Shell.tsx
@@ -1,5 +1,50 @@
 import * as React from 'react'
+import PlayCircle from '@/assets/play_circle.svg'
+import ShellType from '@/types/interfaces'
 
-export default function Shell() {
-  return <div className="aspect-video h-12 w-full rounded-lg bg-muted/50" />
+export default function Shell({ shell }: { shell: ShellType }) {
+  const {
+    shellId,
+    queryStatus,
+    runTime,
+    query,
+    queryType,
+    failMessage,
+    affectedRows,
+    table,
+  } = shell
+
+  const successMessage = `Query OK ${affectedRows} affected (${runTime} sec)`
+
+  const plusButton = (
+    <button type="button" className="flex w-full justify-center bg-primary">
+      <div className="text-bold w-3 text-background">+</div>
+    </button>
+  )
+
+  const resultBox = (
+    <div className="flex w-full flex-col overflow-hidden rounded-sm bg-secondary">
+      <div className="m-3">
+        <p>{queryStatus ? successMessage : failMessage}</p>
+        {queryType === 'SELECT' && <p>{JSON.stringify(table)}</p>}
+      </div>
+      {queryType === 'SELECT' && plusButton}
+    </div>
+  )
+
+  return (
+    <>
+      <div className="flex h-12 w-full items-center overflow-hidden rounded-sm bg-secondary">
+        <button type="button" className="bg-primary p-3">
+          <img src={PlayCircle} alt="play button" />
+        </button>
+        <input
+          type="text"
+          defaultValue={query ?? ''}
+          className="ml-3 h-8 w-full border-none bg-secondary p-2 text-base font-medium text-foreground focus:outline-none"
+        />
+      </div>
+      {runTime != null && resultBox}
+    </>
+  )
 }
