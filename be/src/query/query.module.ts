2c1858ba35cd228af01dd822a38e1b853c8ecab9
성유진 <syj010820@naver.com>
2024-11-10 16:27:21 +0900

    refact: 사용하지 않는 로직 제거

--- be/src/query/query.module.ts
+++ be/src/query/query.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { QueryService } from './query.service';
 import { QueryController } from './query.controller';
 import { QueryDBModule } from '../infrastructure/query-database/query-db.moudle';
-import { SessionModule } from 'src/session/session.module';
 
 @Module({
-  imports: [QueryDBModule, SessionModule],
+  imports: [QueryDBModule],
   controllers: [QueryController],
   providers: [QueryService],
 })
