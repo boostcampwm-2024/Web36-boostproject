d08a227d8ac2d3bb9042bdf09d1f8c5c987bf029
성유진 <syj010820@naver.com>
2024-11-11 10:55:04 +0900

    chore: remove infra directory

--- be/src/infrastructure/query-database/query-db.adapter.ts
+++
@@ -1,7 +0,0 @@
-import { Connection, RowDataPacket } from 'mysql2/promise';
-
-export interface QueryDBAdapter {
-  createConnection(identify: string): Promise<Connection>;
-  closeConnection(connection: Connection): void;
-  run(connection: Connection, query: string): Promise<RowDataPacket[]>;
-}
