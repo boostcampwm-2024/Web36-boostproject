3c9b699ead3a2804b7c0ea58ddecb54de3f36472
KimDayeong <dyyukim@gmail.com>
2024-11-13 22:30:48 +0900

    fix: 유틸 함수 내 잘못된 조건 수정

--- FE/src/api/__mocks__/util.ts
+++ FE/src/api/__mocks__/util.ts
@@ -8,8 +8,8 @@ export default function getMocResult(shell: ShellType) {
   const { queryStatus, runTime, queryType, table } = shell
 
   if (queryStatus === false) return resultError
-  if (table?.length !== 0) return resultPlain
-  if (queryType === 'SELECT' && parseFloat(runTime || '0') < 0.05)
+  if (table?.length === 0) return resultPlain
+  if (queryType === 'SELECT' && parseFloat(runTime || '0') <= 0.05)
     return resultTableShort
 
   return resultTableLong
