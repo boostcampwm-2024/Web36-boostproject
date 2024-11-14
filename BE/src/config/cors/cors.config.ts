1d51df40a93e651f9d9107084082eedd9425fa5f
mintaek22 <biba99@naver.com>
2024-11-14 15:47:14 +0900

    fix:cors 포트 지정

--- BE/src/config/cors/cors.config.ts
+++ BE/src/config/cors/cors.config.ts
@@ -4,7 +4,7 @@ import * as process from 'node:process';
 dotenv.config();
 
 export const corsOptions = {
-  origin: ['http://localhost', process.env.HOST_URL],
+  origin: ['http://localhost:4173', process.env.HOST_URL],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 };
