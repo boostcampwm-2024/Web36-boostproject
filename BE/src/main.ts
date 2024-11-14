867cfe747634930272692ea6ea8a6c321bfcd9ee
mintaek22 <biba99@naver.com>
2024-11-14 14:32:33 +0900

    chore:cors 설정

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './config/swagger/swagger.config';
 import cookieParser from 'cookie-parser';
+import { corsOptions } from './config/cors/cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(corsOptions);
   app.useGlobalPipes(new ValidationPipe());
   setupSwagger(app);
   app.use(cookieParser());
