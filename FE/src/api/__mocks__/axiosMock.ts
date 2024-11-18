1d18bedaf229dc2c7aa8fcbdee4efc48127a3689
KimDayeong <dyyukim@gmail.com>
2024-11-18 11:52:27 +0900

    feat: 실제 환경과 유사한 Axios 인터셉터 추가하여 요청/응답 흐름 처리

--- FE/src/api/__mocks__/axiosMock.ts
+++ FE/src/api/__mocks__/axiosMock.ts
@@ -4,13 +4,18 @@ import { ShellType } from '@/types/interfaces'
 import getMocResult from '@/api/__mocks__/util'
 import shellDataRaw from '@/api/__mocks__/mocData.json'
 
-const shellData: ShellType[] = shellDataRaw.result.map((shell) => ({
-  ...shell,
-  id: String(shell.id), // 초기 데이터에서도 id를 string으로 변환
-}))
+const shellData: ShellType[] = shellDataRaw.result
 
 const axiosMock = axios.create()
 const mock = new MockAdapter(axiosMock)
+
+axiosMock.interceptors.response.use(
+  (response) => {
+    response.data.data = response.data
+    return response
+  },
+  (error) => Promise.reject(error)
+)
 
 // fetch
 mock.onGet('/shells').reply(200, shellData)
@@ -18,14 +23,14 @@ mock.onGet('/shells').reply(200, shellData)
 // add
 mock.onPost('/shells').reply((config) => {
   const newShell: ShellType = JSON.parse(config.data)
-  newShell.id = String(new Date().getTime()) // id를 string으로 변환
+  newShell.id = new Date().getTime()
   shellData.push(newShell)
   return [200, newShell.id]
 })
 
 // execute
 mock.onPost(/^\/shells(\/\d+\/execute)?$/).reply((config) => {
-  const id = config.url!.split('/')[2]
+  const id = parseInt(config.url!.split('/')[2], 10)
   const executeddShell = shellData.find((shell) => shell.id === id)
   const index = shellData.findIndex((shell) => shell.id === id)
 
@@ -38,7 +43,7 @@ mock.onPost(/^\/shells(\/\d+\/execute)?$/).reply((config) => {
 
 // delete
 mock.onDelete(/\/shells\/\d+/).reply((config) => {
-  const id = config.url!.split('/').pop()!
+  const id = parseInt(config.url!.split('/').pop()!, 10)
   const index = shellData.findIndex((shell) => shell.id === id)
 
   if (index === -1) return [404, { error: 'Shell not found' }]
@@ -49,7 +54,7 @@ mock.onDelete(/\/shells\/\d+/).reply((config) => {
 
 // put
 mock.onPut(/\/shells\/\d+/).reply((config) => {
-  const id = config.url!.split('/').pop()!
+  const id = parseInt(config.url!.split('/').pop()!, 10)
   const newQuery = config.data
   const changedShell = shellData.find((shell) => shell.id === id)
 
