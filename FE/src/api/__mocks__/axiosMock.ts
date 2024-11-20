fe99625d63159e4b8e7f7faa11edafe0200eecc3
KimDayeong <dyyukim@gmail.com>
2024-11-20 18:20:02 +0900

    chore: 목업 파일을 api 별로 분리

--- FE/src/api/__mocks__/axiosMock.ts
+++ FE/src/api/__mocks__/axiosMock.ts
@@ -1,10 +1,12 @@
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
-import mockShells from '@/api/__mocks__/mocShells'
+import mockShells from '@/api/__mocks__/mockShells'
+import mockTables from '@/api/__mocks__/mockTables'
 
 const axiosMock = axios.create()
 const mock = new MockAdapter(axiosMock)
 
 mockShells(mock) // shells 관련 API 모킹
+mockTables(mock) // tables 관련 API 모킹
 
 export default axiosMock
