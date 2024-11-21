8a9a6de8c477b07e063856ddb360d32ea21a3ff0
unknown <wkdtmdgnsqn@naver.com>
2024-11-21 15:14:45 +0900

    fix : pm2 dist 폴더 디렉토리 경로 수정

--- BE/ecosystem.config.js
+++ BE/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: 'nest-server',
-        script: 'BE/dist/main.js', // NestJS 서버의 진입 파일
+        script: 'dist/main.js', // NestJS 서버의 진입 파일
         instances: 'max',       // 클러스터 모드: CPU 코어 수만큼 프로세스 실행
         exec_mode: 'cluster',   // 클러스터 모드
         watch: true,            // 파일 변경 감지 후 재시작
