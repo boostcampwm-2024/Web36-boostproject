5d1c5b7448279999a2784b0c1606d1863948bf95
KimDayeong <dyyukim@gmail.com>
2024-11-23 13:54:04 +0900

    feat: generate query 로직 구현

--- FE/src/components/TableTool.tsx
+++ FE/src/components/TableTool.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import {
   Select,
   SelectContent,
@@ -30,21 +30,65 @@ import { Input } from '@/components/ui/input'
 import Label from '@/components/ui/label'
 import { Plus, Minus } from 'lucide-react'
 
-import { TableType, TableColumnType } from '@/types/interfaces'
+import {
+  TableType,
+  TableToolType,
+  TableToolColumnType,
+} from '@/types/interfaces'
 import { COLUMN_TYPES } from '@/constants'
-import generateKey from '@/util'
+import {
+  generateKey,
+  convertTableData,
+  generateCreateTableQuery,
+  generateAlterTableQuery,
+} from '@/util'
+import EditableInput from './EditableInput'
 
 export default function TableTool({
   tableData = [],
 }: {
   tableData: TableType[]
 }) {
-  const [selectedTable, setSelectedTable] = useState(tableData[0] || null)
+  const tableToolData = convertTableData(tableData)
+
+  const initialTableData = useRef<TableToolType[]>([])
+  const [selectedTable, setSelectedTable] = useState(tableToolData[0] || null)
+
+  useEffect(() => {
+    initialTableData.current = tableToolData
+  }, [tableToolData])
+
+  const handleOnChange = (row: number, id: string, value: unknown) => {
+    const updatedColumns = [...selectedTable.columns]
+
+    updatedColumns[row] = {
+      ...updatedColumns[row],
+      [id]: value,
+    }
+
+    setSelectedTable({
+      ...selectedTable,
+      columns: updatedColumns,
+    })
+  }
+
+  const handleSubmit = () => {
+    if (!selectedTable) return
+    const previousTable = initialTableData.current.find(
+      (table) => table.tableName === selectedTable.tableName
+    )
+
+    const query = previousTable
+      ? generateAlterTableQuery(previousTable, selectedTable)
+      : generateCreateTableQuery(selectedTable)
+
+    console.log(previousTable, query)
+  }
 
   return (
     <>
       <div className="sticky top-0 items-center gap-3 border-b p-2">
-        {tableData?.map((table) => (
+        {tableToolData?.map((table) => (
           <Badge
             variant={
               selectedTable?.tableName === table.tableName
@@ -65,6 +109,7 @@ export default function TableTool({
         <Table>
           <TableHeader className="bg-secondary">
             <TableRow>
+              <TableHead />
               <TableHead>Name</TableHead>
               <TableHead>Type</TableHead>
               <TableHead>PK</TableHead>
@@ -74,26 +119,39 @@ export default function TableTool({
             </TableRow>
           </TableHeader>
           <TableBody>
-            {selectedTable.columns.map((row: TableColumnType) => (
+            {selectedTable.columns.map((row: TableToolColumnType, rowIdx) => (
               <TableRow key={generateKey(row)}>
+                <TableCell>
+                  <Checkbox />
+                </TableCell>
                 {Object.entries(row).map(
                   ([key, value]) =>
-                    ['name', 'type', 'PK', 'UQ', 'AI', 'NN'].includes(key) && (
+                    key !== 'id' && (
                       <TableCell key={generateKey(value)}>
                         {key === 'name' && (
-                          <Input
-                            type="text"
-                            value={value}
-                            className="h-[32px] w-[70px] p-2"
+                          <EditableInput
+                            value={String(value)}
+                            rowIdx={rowIdx}
+                            handleOnChange={handleOnChange}
                           />
                         )}
 
                         {typeof value === 'boolean' && (
-                          <Checkbox checked={value} />
+                          <Checkbox
+                            checked={value}
+                            onCheckedChange={(checked) =>
+                              handleOnChange(rowIdx, key, checked)
+                            }
+                          />
                         )}
 
                         {key === 'type' && (
-                          <Select>
+                          <Select
+                            value={value}
+                            onValueChange={(newValue) =>
+                              handleOnChange(rowIdx, 'type', newValue)
+                            }
+                          >
                             <SelectTrigger className="h-[32px] w-[120px] p-2">
                               <SelectValue placeholder={value} />
                             </SelectTrigger>
@@ -149,7 +207,7 @@ export default function TableTool({
             </DialogFooter>
           </DialogContent>
         </Dialog>
-        <Button variant="default" className="ml-3 h-8">
+        <Button variant="default" className="ml-3 h-8" onClick={handleSubmit}>
           Generate Query
         </Button>
       </div>
