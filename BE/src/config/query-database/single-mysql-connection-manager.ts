ada46c4ef6e29fd0eb00ff039ec11dcaefb6428e
장승훈 <hoon@jangseunghun-ui-MacBookAir.local>
2024-11-24 16:09:18 +0900

    refactor: 구현체 내부에서 실제 로직이 노출되도록 리팩토링

--- BE/src/config/query-database/single-mysql-connection-manager.ts
+++ BE/src/config/query-database/single-mysql-connection-manager.ts
@@ -1,18 +1,6 @@
-import {
-  ConflictException,
-  Injectable,
-  InternalServerErrorException,
-  OnModuleInit,
-} from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { createReadStream } from 'fs';
-import {
-  Connection,
-  ConnectionOptions,
-  createConnection,
-  createPool,
-  Pool,
-} from 'mysql2/promise';
+import { Connection, createPool, Pool } from 'mysql2/promise';
 
 @Injectable()
 export class SingleMySQLConnectionManager implements OnModuleInit {
@@ -25,14 +13,6 @@ export class SingleMySQLConnectionManager implements OnModuleInit {
     this.createAdminConnection();
   }
 
-  public getConnection(identify: string): Connection {
-    return this.userConnectionList[identify];
-  }
-
-  public getAdminPool() {
-    return this.adminConnection;
-  }
-
   private createAdminConnection() {
     this.adminConnection = createPool({
       host: this.configService.get<string>('QUERY_DB_HOST'),
@@ -43,49 +23,16 @@ export class SingleMySQLConnectionManager implements OnModuleInit {
     });
   }
 
-  public async createConnection(identify: string) {
-    if (!this.getConnection(identify)) {
-      this.userConnectionList[identify] = await createConnection({
-        host: this.configService.get<string>('QUERY_DB_HOST'),
-        user: identify.substring(0, 10),
-        password: identify,
-        port: this.configService.get<number>('QUERY_DB_PORT', 3306),
-        database: identify,
-        infileStreamFactory: (path) => {
-          return createReadStream(path);
-        },
-      } as ConnectionOptions);
-    }
+  public getConnection(identify: string): Connection {
+    return this.userConnectionList[identify];
   }
 
-  public async initUserDatabase(identify: string) {
-    try {
-      const connectInfo = {
-        name: identify.substring(0, 10),
-        password: identify,
-        host: '%',
-        database: identify,
-      };
+  public setConnection(identify: string, connection: Connection) {
+    this.userConnectionList[identify] = connection;
+  }
 
-      await this.adminConnection.query(
-        `create database ${connectInfo.database};`,
-      );
-      await this.adminConnection.query(
-        `create user '${connectInfo.name}'@'${connectInfo.host}' identified by '${connectInfo.password}';`,
-      );
-      await this.adminConnection.query(
-        `grant all privileges on ${connectInfo.database}.* to '${connectInfo.name}'@'${connectInfo.host}';`,
-      );
-    } catch (error) {
-      if (error.code === 'ER_DB_CREATE_EXISTS') {
-        throw new ConflictException(
-          `Database already exists for user: ${identify}`,
-        );
-      }
-      throw new InternalServerErrorException(
-        `Database initialization failed for user: ${identify}`,
-      );
-    }
+  public getAdminPool() {
+    return this.adminConnection;
   }
 
   public async closeConnection(identify: string) {
--- BE/src/config/query-database/single-mysql.adapter.ts
+++ BE/src/config/query-database/single-mysql.adapter.ts
@@ -1,15 +1,29 @@
-import { Injectable, Scope, Inject } from '@nestjs/common';
+import {
+  Injectable,
+  Scope,
+  Inject,
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { QueryDBAdapter } from './query-db.adapter';
 import { Request } from 'express';
-import { Connection, Pool } from 'mysql2/promise';
+import {
+  Connection,
+  ConnectionOptions,
+  createConnection,
+  Pool,
+} from 'mysql2/promise';
 import { REQUEST } from '@nestjs/core';
 import { SingleMySQLConnectionManager } from './single-mysql-connection-manager';
+import { ConfigService } from '@nestjs/config';
+import { createReadStream } from 'fs';
 
 @Injectable({ scope: Scope.REQUEST })
 export class SingleMySQLAdapter implements QueryDBAdapter {
   constructor(
     private singleMySQLConnectionManager: SingleMySQLConnectionManager,
     @Inject(REQUEST) private readonly request: Request,
+    private readonly configService: ConfigService,
   ) {}
 
   private getSID(): string {
@@ -25,16 +39,58 @@ export class SingleMySQLAdapter implements QueryDBAdapter {
   }
 
   public async initUserDatabase() {
-    await this.singleMySQLConnectionManager.initUserDatabase(this.getSID());
+    const adminConnection = this.singleMySQLConnectionManager.getAdminPool();
+    const identify = this.getSID();
+    try {
+      const connectInfo = {
+        name: identify.substring(0, 10),
+        password: identify,
+        host: '%',
+        database: identify,
+      };
+
+      await adminConnection.query(`create database ${connectInfo.database};`);
+      await adminConnection.query(
+        `create user '${connectInfo.name}'@'${connectInfo.host}' identified by '${connectInfo.password}';`,
+      );
+      await adminConnection.query(
+        `grant all privileges on ${connectInfo.database}.* to '${connectInfo.name}'@'${connectInfo.host}';`,
+      );
+    } catch (error) {
+      if (error.code === 'ER_DB_CREATE_EXISTS') {
+        throw new ConflictException(
+          `Database already exists for user: ${identify}`,
+        );
+      }
+      throw new InternalServerErrorException(
+        `Database initialization failed for user: ${identify}`,
+      );
+    }
   }
 
   public async createConnection() {
-    await this.singleMySQLConnectionManager.createConnection(this.getSID());
+    const identify = this.getSID();
+    let userConnection = this.singleMySQLConnectionManager.getConnection(
+      this.getSID(),
+    );
+    if (!userConnection) {
+      userConnection = await createConnection({
+        host: this.configService.get<string>('QUERY_DB_HOST'),
+        user: identify.substring(0, 10),
+        password: identify,
+        port: this.configService.get<number>('QUERY_DB_PORT', 3306),
+        database: identify,
+        infileStreamFactory: (path) => {
+          return createReadStream(path);
+        },
+      } as ConnectionOptions);
+      this.singleMySQLConnectionManager.setConnection(identify, userConnection);
+    }
     await this.run('set profiling = 1;');
   }
 
   public async closeConnection() {
-    await this.singleMySQLConnectionManager.createConnection(this.getSID());
+    await this.singleMySQLConnectionManager.closeConnection(this.getSID());
     await this.removeDatabaseInfo();
     this.singleMySQLConnectionManager.deleteConnection(this.getSID());
   }
