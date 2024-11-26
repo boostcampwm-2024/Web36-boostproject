268b2ab320d0b20af2847bfe42e920a01d8d97ec
장승훈 <hoon@jangseunghun-ui-MacBookAir.local>
2024-11-26 15:01:51 +0900

    feat : 스토리지 사용량 UI 레이아웃

--- FE/src/components/ShellList.tsx
+++ FE/src/components/ShellList.tsx
@@ -2,20 +2,35 @@ import { Badge } from '@/components/ui/badge'
 import Shell from '@/components/Shell'
 import { ShellType } from '@/types/interfaces'
 import useShellHandlers from '@/hooks/useShellHandler'
+import CapacityUsage from './CapacityUsage'
+
+function ButtonGroup({ onAddShell }: { onAddShell: () => void }) {
+  return (
+    <div className="flex items-center gap-2">
+      <Badge variant="outline" className="cursor-pointer" onClick={onAddShell}>
+        + query
+      </Badge>
+    </div>
+  )
+}
 
 export default function ShellList({ shells = [] }: { shells: ShellType[] }) {
   const { addShell } = useShellHandlers()
 
   return (
     <>
-      <div className="sticky top-0 flex shrink-0 items-center gap-3 border-b bg-background p-2">
-        <Badge variant="outline" className="cursor-pointer" onClick={addShell}>
-          + query
-        </Badge>
+      <div className="sticky top-0 flex items-center justify-between border-b bg-background p-2">
+        {/* 버튼 그룹 */}
+        <ButtonGroup onAddShell={addShell} />
+
+        {/* 점유율 컴포넌트 */}
+        <CapacityUsage used={400} total={1000} unit="GB" />
       </div>
       {shells.length > 0 && (
         <div className="flex flex-1 flex-col gap-3 p-4">
-          {shells?.map((shell) => <Shell key={shell.id} shell={shell} />)}
+          {shells.map((shell) => (
+            <Shell key={shell.id} shell={shell} />
+          ))}
         </div>
       )}
     </>
