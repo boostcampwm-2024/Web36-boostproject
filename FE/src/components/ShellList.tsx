e0e5b481a93981de48eee8ff9cfc8a54ccf62fff
장승훈 <hoon@jangseunghun-ui-MacBookAir.local>
2024-11-26 15:32:09 +0900

    refactor: 버튼 컴포넌트로 묶지 않고 하나이 컴포넌트에서 관리하도록 변경;

--- FE/src/components/ShellList.tsx
+++ FE/src/components/ShellList.tsx
@@ -4,27 +4,24 @@ import { ShellType } from '@/types/interfaces'
 import useShellHandlers from '@/hooks/useShellHandler'
 import CapacityUsage from './CapacityUsage'
 
-function ButtonGroup({ onAddShell }: { onAddShell: () => void }) {
-  return (
-    <div className="flex items-center gap-2">
-      <Badge variant="outline" className="cursor-pointer" onClick={onAddShell}>
-        + query
-      </Badge>
-    </div>
-  )
-}
-
 export default function ShellList({ shells = [] }: { shells: ShellType[] }) {
   const { addShell } = useShellHandlers()
-
   return (
     <>
       <div className="sticky top-0 flex items-center justify-between border-b bg-background p-2">
         {/* 버튼 그룹 */}
-        <ButtonGroup onAddShell={addShell} />
+        <div className="flex items-center gap-2">
+          <Badge
+            variant="outline"
+            className="cursor-pointer"
+            onClick={addShell}
+          >
+            + query
+          </Badge>
+        </div>
 
         {/* 점유율 컴포넌트 */}
-        <CapacityUsage used={400} total={1000} unit="GB" />
+        <CapacityUsage used={800} total={1000} unit="GB" isLoading />
       </div>
       {shells.length > 0 && (
         <div className="flex flex-1 flex-col gap-3 p-4">
