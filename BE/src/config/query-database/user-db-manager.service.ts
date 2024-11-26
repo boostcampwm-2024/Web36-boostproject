d89af10b8fc22b0c279761a68db9d8242b86ed6b
mintaek22 <biba99@naver.com>
2024-11-26 15:33:47 +0900

    fix: 쿼리 에러시에도 db connection 파기 하도록 변경

--- BE/src/config/query-database/user-db-manager.service.ts
+++ BE/src/config/query-database/user-db-manager.service.ts
@@ -1,35 +1,31 @@
-import {Inject, Injectable, OnModuleDestroy, Scope} from '@nestjs/common';
+import {Inject, Injectable, Scope} from '@nestjs/common';
 import {Request} from 'express';
-import {Connection, createConnection, QueryResult} from 'mysql2/promise';
+import {createConnection, QueryResult} from 'mysql2/promise';
 import {ConfigService} from '@nestjs/config';
 import {REQUEST} from '@nestjs/core';
 
 @Injectable({ scope: Scope.TRANSIENT })
-export class UserDBManager implements OnModuleDestroy {
-  private connection: Connection;
+export class UserDBManager{
 
   constructor(
       private readonly configService: ConfigService,
       @Inject(REQUEST) private readonly request: Request,
   ) {}
 
-  onModuleDestroy() {
-    if (this.connection) {
-      this.connection.end();
-    }
-  }
-
   async run(query: string) : Promise<QueryResult> {
-    if (!this.connection) {
-      await this.createConnection();
+    const connection = await this.createConnection();
+    try{
+      const [result] = await connection.query(query);
+      return result;
     }
-    const [result] = await this.connection.query(query);
-    return result;
+    finally {
+      await connection.end();
+    }
   }
 
   private async createConnection() {
     const identify = this.request.sessionID;
-    this.connection = await createConnection({
+    return createConnection({
       host: this.configService.get<string>('QUERY_DB_HOST'),
       user: identify.substring(0, 10),
       password: identify,
--- BE/src/query/query.module.ts
+++ BE/src/query/query.module.ts
@@ -1,10 +1,9 @@
-import { Module } from '@nestjs/common';
-import { QueryService } from './query.service';
-import { QueryController } from './query.controller';
-import { AdminQueryDBModule } from '../config/query-database/admin-query-db.moudle';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Shell } from '../shell/shell.entity';
-import { ShellService } from '../shell/shell.service';
+import {Module} from '@nestjs/common';
+import {QueryService} from './query.service';
+import {QueryController} from './query.controller';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {Shell} from '../shell/shell.entity';
+import {ShellService} from '../shell/shell.service';
 import {UserDBManager} from "../config/query-database/user-db-manager.service";
 
 @Module({
