544b119e468418faaef06eaffd1bb8100d5c97ef
Hoon <wkdtmdgnsqn@naver.com>
2024-11-29 00:31:04 +0900

    fix : 예시 쿼리 변경

--- FE/src/constants/exampleQuery.ts
+++ FE/src/constants/exampleQuery.ts
@@ -3,8 +3,8 @@ import { ExampleQuery } from '@/types/interfaces'
 const createQueryExample = `CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,       -- 고유 ID, 기본 키
     name VARCHAR(100) NOT NULL,              -- 이름
-    city VARCHAR(100) NOT NULL,              -- 도시
-    email VARCHAR(255) UNIQUE NOT NULL,      -- 이메일, 고유값
+    city VARCHAR(100),                -- 도시 이름
+    email VARCHAR(255) NOT NULL,      -- 이메일, 고유값
     phone VARCHAR(20) NOT NULL,              -- 전화번호
     age INT CHECK (age >= 0),                -- 나이 (0 이상만 허용)
     gender ENUM('Male', 'Female', 'Other') NOT NULL  -- 성별 (열거형)
