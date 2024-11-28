a96016a4ad82204e14a3aabc9c813d1c19ecc481
mintaek22 <biba99@naver.com>
2024-11-29 00:33:42 +0900

    fix: 랜덤데이터 삽입도 redis에 데이터 반영

--- BE/src/record/record.module.ts
+++ BE/src/record/record.module.ts
@@ -3,9 +3,10 @@ import { RecordController } from './record.controller';
 import { RecordService } from './record.service';
 import { RedisModule } from '../config/redis/redis.module';
 import { QueryDBModule } from '../config/query-database/query-db.moudle';
+import { UsageModule } from '../usage/usage.module';
 
 @Module({
-  imports: [RedisModule, QueryDBModule],
+  imports: [RedisModule, QueryDBModule, UsageModule],
   controllers: [RecordController],
   providers: [RecordService],
 })
