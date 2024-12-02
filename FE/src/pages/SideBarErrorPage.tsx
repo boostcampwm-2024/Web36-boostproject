22f104110bb7bde90d373b0c199537ea63cf3721
KimDayeong <dyyukim@gmail.com>
2024-12-02 22:47:34 +0900

    feat: 사이드바 에러 페이지 구현

---
+++ FE/src/pages/SideBarErrorPage.tsx
@@ -0,0 +1,25 @@
+import { Button } from '@/components/ui/button'
+
+type SidebarErrorPageProps = {
+  resetErrorBoundary: () => void
+}
+
+export default function SidebarErrorPage({
+  resetErrorBoundary,
+}: SidebarErrorPageProps) {
+  return (
+    <div className="flex w-full max-w-[25vw] flex-col items-center justify-center bg-secondary p-4 text-muted-foreground">
+      <h2 className="text-xl font-semibold text-primary">Oops!</h2>
+      <p className="my-2 text-center text-sm">
+        Something went wrong in the sidebar.
+      </p>
+      <Button
+        variant="outline"
+        className="mt-4 h-8 text-sm"
+        onClick={resetErrorBoundary}
+      >
+        Reload Sidebar
+      </Button>
+    </div>
+  )
+}
