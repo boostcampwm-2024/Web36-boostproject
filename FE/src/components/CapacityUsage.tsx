5632337f35dab96a3aa6df1c7291017f10566248
KimDayeong <dyyukim@gmail.com>
2024-12-02 22:05:45 +0900

    chore: 불필요한 주석 제거

--- FE/src/components/CapacityUsage.tsx
+++ FE/src/components/CapacityUsage.tsx
@@ -22,28 +22,22 @@ function CapacityUsage({ usage }: { usage: UsageType }) {
 
   return (
     <div className="flex max-w-md items-center space-x-4 rounded-lg bg-white">
-      {/* 게이지 바 */}
       <div className="w-24 flex-grow">
         <div className="h-1.5 w-full overflow-hidden rounded-full bg-gray-200">
           <div
             className={`h-full ${color}`}
             style={{ width: `${percentage}%` }}
-          ></div>
+          />
         </div>
       </div>
-
-      {/* 텍스트 영역 */}
       <div className="flex-shrink-0 text-left text-xs font-semibold text-muted-foreground">
         <div className="flex items-center space-x-1">
-          {/* 현재 사용량 */}
           {loading ? (
-            <div className="mb-0.5 h-3 w-3 animate-spin rounded-full border-2 border-gray-300 border-t-blue-500"></div>
+            <div className="mb-0.5 h-3 w-3 animate-spin rounded-full border-2 border-gray-300 border-t-blue-500" />
           ) : (
             <span>{used}</span>
           )}
-          {/* 단위 */}
           <span>{UNIT}</span>
-          {/* 총 사용량 */}
           <span>
             / {total} {UNIT}
           </span>
