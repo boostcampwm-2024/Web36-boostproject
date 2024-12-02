a59568441a06b481271c4ab606a0e448297b4451
KimDayeong <dyyukim@gmail.com>
2024-12-02 14:44:19 +0900

    chore: ace editor 동적 경로 지정

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App'
+import * as ace from 'ace-builds'
 import './index.css'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 
 const queryClient = new QueryClient()
+ace.config.set('basePath', '/node_modules/ace-builds/src-noconflict')
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
