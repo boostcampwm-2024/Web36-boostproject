dbecf656e69b57df21449a07868a3fa3d04180ac
KimDayeong <dyyukim@gmail.com>
2024-12-02 11:55:35 +0900

    fix: 타입스크립트 에러 해결

--- FE/src/error/toastErrorHandler.ts
+++ FE/src/error/toastErrorHandler.ts
@@ -5,37 +5,39 @@ export default function useToastErrorHandler() {
   const { toast } = useToast()
 
   return (error: AxiosError | Error) => {
-    let title
-    let description
+    let title: string | undefined
+    let description: string | undefined
 
     if ('isAxiosError' in error && error.isAxiosError) {
       const status = error.response?.status
+      const responseData = error.response?.data as {
+        error?: { message?: string }
+      }
+
       switch (status) {
         case 429:
           title = 'Server Connection Limit Exceeded'
           description =
-            error.response?.data?.error?.message ||
+            responseData?.error?.message ||
             'The server is currently handling too many requests. Please try again later.'
           break
         case 422:
           title = 'Usage Limit Exceeded'
           description =
-            error.response?.data?.error?.message ||
+            responseData?.error?.message ||
             'You have exceeded your usage limit. Please delete rows or tables and try again.'
           break
         default:
           console.error('Unhandled AxiosError:', error)
           title = `Error ${status || 'Unknown'}`
           description =
-            error.response?.data?.error?.message || 'An unknown error occurred.'
+            responseData?.error?.message || 'An unknown error occurred.'
       }
     } else {
       console.error('Unhandled Error:', error)
       title = 'An unexpected error occurred'
-      message =
-        error.message ||
-        error.response?.data?.error?.message ||
-        'Please try again later.'
+      description =
+        error.message || 'An unknown error occurred. Please try again later.'
     }
 
     toast({
