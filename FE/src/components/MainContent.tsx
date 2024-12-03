c35ce43b10bc4896a4d2fc8287a4133c57463aa6
KimDayeong <dyyukim@gmail.com>
2024-12-03 14:13:48 +0900

    feat: 메인 페이지에 로딩 페이지 추가

--- FE/src/components/MainContent.tsx
+++ FE/src/components/MainContent.tsx
@@ -4,6 +4,7 @@ import Shell from '@/components/common/shell/Shell'
 import { useShells } from '@/hooks/query/useShellQuery'
 import useUsages from '@/hooks/query/useUsageQuery'
 import useShellHandlers from '@/hooks/useShellHandler'
+import LoadingPage from '@/pages/LoadingPage'
 
 import { ErrorBoundary } from 'react-error-boundary'
 import ShellErrorFallback from './common/shell/ShellErrorFallback'
@@ -34,17 +35,21 @@ export default function MainContent() {
           />
         </div>
       </div>
-      <div className="flex flex-1 flex-col gap-3 p-4">
-        {shells.data?.map((shell) => (
-          <ErrorBoundary
-            key={shell.id}
-            FallbackComponent={ShellErrorFallback}
-            onReset={() => window.location.reload()}
-          >
-            <Shell key={shell.id} shell={shell} />
-          </ErrorBoundary>
-        ))}
-      </div>
+      {shells.isLoading ? (
+        <LoadingPage />
+      ) : (
+        <div className="flex flex-1 flex-col gap-3 p-4">
+          {shells.data?.map((shell) => (
+            <ErrorBoundary
+              key={shell.id}
+              FallbackComponent={ShellErrorFallback}
+              onReset={() => window.location.reload()}
+            >
+              <Shell key={shell.id} shell={shell} />
+            </ErrorBoundary>
+          ))}
+        </div>
+      )}
     </>
   )
 }
