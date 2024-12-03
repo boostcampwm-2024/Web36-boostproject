86d144023d2706dfd485b5497f957ccb0a3f404b
KimDayeong <dyyukim@gmail.com>
2024-12-03 10:48:37 +0900

    chore: 메인 페이지 컴포넌트 이름 수정

--- FE/src/pages/MainPage.tsx
+++ FE/src/pages/MainPage.tsx
@@ -10,7 +10,7 @@ import { useTables } from '@/hooks/query/useTableQuery'
 import { ErrorBoundary } from 'react-error-boundary'
 import MainErrorPage from './MainErrorPage'
 
-export default function Page() {
+export default function MainPage() {
   const [activeItem, setActiveItem] = useState(MENU[0])
   const tables = useTables()
 
