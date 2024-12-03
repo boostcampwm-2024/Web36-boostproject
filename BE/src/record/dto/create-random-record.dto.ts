1324d387a3090d8d29fea2454c0f6450d6a63f33
Hoon <wkdtmdgnsqn@naver.com>
2024-12-03 16:40:25 +0900

    feature : zod 와 랜덤데이터 추가 api 연동

--- BE/src/record/dto/create-random-record.dto.ts
+++ BE/src/record/dto/create-random-record.dto.ts
@@ -1,71 +1,9 @@
-import { Type } from 'class-transformer';
+import { createZodDto } from 'nestjs-zod';
 import {
-  ArrayMaxSize,
-  IsArray,
-  IsEnum,
-  IsInt,
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsString,
-  Max,
-  Min,
-  ValidateIf,
-  ValidateNested,
-} from 'class-validator';
+  RandomColumnInfo,
+  CreateRandomRecordDtoSchema,
+} from '../../../../schemas/index';
 
-export enum Domains {
-  NAME = 'name',
-  COUNTRY = 'country',
-  CITY = 'city',
-  EMAIL = 'email',
-  PHONE = 'phone',
-  SEX = 'sex',
-  BOOLEAN = 'boolean',
-  NUMBER = 'number',
-  ENUM = 'enum',
-}
+class CreateRandomRecordDto extends createZodDto(CreateRandomRecordDtoSchema) {}
 
-export class CreateRandomRecordDto {
-  @IsString()
-  @IsNotEmpty()
-  tableName: string;
-
-  @IsArray()
-  @ValidateNested({ each: true })
-  @Type(() => RandomColumnInfo)
-  columns: RandomColumnInfo[];
-
-  @IsInt()
-  @Min(1)
-  @Max(100000)
-  count: number;
-}
-
-export class RandomColumnInfo {
-  @IsString()
-  @IsNotEmpty()
-  name: string;
-
-  @IsEnum(Domains)
-  type: Domains;
-
-  @IsInt()
-  @Min(0)
-  @Max(100)
-  blank: number;
-
-  @IsOptional()
-  @IsNumber()
-  min?: number;
-
-  @IsOptional()
-  @IsNumber()
-  max?: number;
-
-  @ValidateIf((obj) => obj.enum !== undefined)
-  @IsArray()
-  @ArrayMaxSize(10)
-  @IsString({ each: true })
-  enum?: string[];
-}
+export { RandomColumnInfo, CreateRandomRecordDto };
