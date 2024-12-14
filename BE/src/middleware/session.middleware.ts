0bb1b63722d08eef171fb8217ca8d4163a693457
mintaek22 <biba99@naver.com>
2024-12-14 20:56:09 +0900

    refactor: middleware error next로 던지도록 변경

--- BE/src/middleware/session.middleware.ts
+++ BE/src/middleware/session.middleware.ts
@@ -5,14 +5,12 @@ import { v4 as uuidv4 } from 'uuid';
 import { CustomRedisStore } from 'src/config/redis/custom-redis-store';
 import { RedisService } from 'src/config/redis/redis.service';
 import { ConfigService } from '@nestjs/config';
-import { LoggerService } from 'src/config/logger/logger.service';
 
 @Injectable()
 export class SessionMiddleware implements NestMiddleware {
   constructor(
     private readonly redisService: RedisService,
     private readonly configService: ConfigService,
-    private readonly loggerService: LoggerService,
   ) {}
 
   use(req: Request, res: Response, next: NextFunction) {
@@ -31,20 +29,7 @@ export class SessionMiddleware implements NestMiddleware {
         await this.redisService.setNewSession(req.sessionID);
         next();
       } catch (error) {
-        res.status(500).json({
-          status: false,
-          error: {
-            code: 500,
-            message: error.message,
-          },
-        });
-        this.loggerService.error({
-          message: error.message,
-          stack: error.stack,
-          timestamp: new Date().toLocaleString('ko-KR', {
-            timeZone: 'Asia/Seoul',
-          }),
-        });
+        next(error);
       }
     });
   }
